fceaf119271ecc4a0e15cb787504589a7179def8
PiMoNFeeD <36343425+PiMoNFeeD@users.noreply.github.com>
2020-12-31 15:08:31 +0300

    possible fix for ironsight config initialization

--- src/game/shared/cstrike/weapon_csbase.h
+++ src/game/shared/cstrike/weapon_csbase.h
@@ -349,6 +349,7 @@ public:
 	void				 UpdateIronSightController( void );
 	CIronSightController *m_IronSightController;
 	CNetworkVar( int, m_iIronSightMode );
+	CNetworkVar( bool, m_bIronsightInitiallized );
 #endif //IRONSIGHT
 };
 
