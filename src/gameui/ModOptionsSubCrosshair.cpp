2815fb0fdb20c8baa83963fbbbe509bc307db6c3
PiMoNFeeD <36343425+PiMoNFeeD@users.noreply.github.com>
2021-01-31 22:24:47 +0300

    some updates to ModOptionsSubCrosshair
    main code is done but the gap is inaccurate

--- src/gameui/ModOptionsSubCrosshair.cpp
+++ src/gameui/ModOptionsSubCrosshair.cpp
@@ -56,12 +56,6 @@
 
 using namespace vgui;
 
-
-#define DEFAULT_SUIT_HUE 30
-#define DEFAULT_PLATE_HUE 6
-
-void UpdateLogoWAD( void *hdib, int r, int g, int b );
-
 struct ColorItem_t
 {
 	const char	*name;
@@ -76,7 +70,14 @@ static ColorItem_t s_crosshairColors[] =
 	{ "#Valve_Yellow",	250,	250,	50 },
 	{ "#Valve_Ltblue",	50,		250,	250 },
 };
-static const int NumCrosshairColors = ARRAYSIZE(s_crosshairColors);
+
+static const char* crosshairBackgroundImages[] =
+{
+	"crosshair/de_dust2",
+	"crosshair/de_mirage",
+	"crosshair/de_aztec",
+	"crosshair/cs_office",
+};
 
 
 //-----------------------------------------------------------------------------
@@ -96,20 +97,24 @@ protected:
 
 	virtual void Paint();
 	void DrawCrosshairRect( int r, int g, int b, int a, int x0, int y0, int x1, int y1, bool bAdditive );
-	void InitCrosshairColorEntries();
 	void UpdateCrosshair();
 
 private:
 	CModOptionsSubCrosshair	*m_pOptionsPanel;
-	vgui::ComboBox			*m_pColorComboBox;
-	CLabeledCommandComboBox	*m_pStyleComboBox;
-	CCvarToggleCheckButton	*m_pOutlineCheckbox;
-	CCvarSlider				*m_pOutlineSlider;
-	CCvarToggleCheckButton	*m_pDotCheckbox;
-	CCvarToggleCheckButton	*m_pAlphaCheckbox;
-	int m_R, m_G, m_B;
-	float m_barSize;
-	float m_barThickness;
+	CLabeledCommandComboBox	*m_pCrosshairStyle;
+	CCvarSlider				*m_pCrosshairAlpha;
+	CCvarToggleCheckButton	*m_pCrosshairUseAlpha;
+	CCvarSlider				*m_pCrosshairGap;
+	CCvarToggleCheckButton	*m_pCrosshairGapUseWeaponValue;
+	CCvarSlider				*m_pCrosshairSize;
+	CCvarSlider				*m_pCrosshairThickness;
+	CCvarToggleCheckButton	*m_pCrosshairDot;
+	CCvarSlider				*m_pCrosshairColorR;
+	CCvarSlider				*m_pCrosshairColorG;
+	CCvarSlider				*m_pCrosshairColorB;
+	CCvarToggleCheckButton	*m_pCrosshairDrawOutline;
+	CCvarSlider				*m_pCrosshairOutlineThickness;
+	CCvarToggleCheckButton	*m_pCrosshairT;
 	int m_iCrosshairTextureID;
 };
 
@@ -117,26 +122,41 @@ private:
 CrosshairImagePanelCS::CrosshairImagePanelCS( Panel *parent, const char *name, CModOptionsSubCrosshair* pOptionsPanel ) : CrosshairImagePanelBase( parent, name )
 {
 	m_pOptionsPanel = pOptionsPanel;
-	m_pColorComboBox = new ComboBox(m_pOptionsPanel, "CrosshairColorComboBox", NumCrosshairColors, false);
-	m_pStyleComboBox = new CLabeledCommandComboBox( m_pOptionsPanel, "CrosshairStyleComboBox" );
-	m_pOutlineCheckbox = new CCvarToggleCheckButton( m_pOptionsPanel, "CrosshairOutlineCheckBox", "#GameUI_Crosshair_Outline", "cl_crosshair_drawoutline" );
-	m_pOutlineSlider = new CCvarSlider( m_pOptionsPanel, "CrosshairOutlineSlider", "#GameUI_Crosshair_Outline_Thickness", 0.1f, 3.0f, "cl_crosshair_outlinethickness" );
-	m_pAlphaCheckbox = new CCvarToggleCheckButton( m_pOptionsPanel, "CrosshairAlphaCheckbox", "#GameUI_Crosshair_AlphaCheckBox", "cl_crosshairusealpha" );
+	m_pCrosshairStyle = new CLabeledCommandComboBox( m_pOptionsPanel, "CrosshairStyle" );
+	m_pCrosshairAlpha = new CCvarSlider( m_pOptionsPanel, "CrosshairAlpha", "#GameUI_Crosshair_Alpha", 0.0f, 255.0f, "cl_crosshairalpha" );
+	m_pCrosshairUseAlpha = new CCvarToggleCheckButton( m_pOptionsPanel, "CrosshairUseAlpha", "#GameUI_Crosshair_UseAlpha", "cl_crosshairusealpha" );
+	m_pCrosshairGap = new CCvarSlider( m_pOptionsPanel, "CrosshairGap", "#GameUI_Crosshair_Gap", 0.1f, 10.0f, "cl_crosshairgap" );
+	m_pCrosshairGapUseWeaponValue = new CCvarToggleCheckButton( m_pOptionsPanel, "CrosshairGapUseWeaponValue", "#GameUI_Crosshair_Gap_UseWeaponValue", "cl_crosshairgap_useweaponvalue" );
+	m_pCrosshairSize = new CCvarSlider( m_pOptionsPanel, "CrosshairSize", "#GameUI_Crosshair_Size", 0.1f, 10.0f, "cl_crosshairsize" );
+	m_pCrosshairThickness = new CCvarSlider( m_pOptionsPanel, "CrosshairThickness", "#GameUI_Crosshair_Thickness", 0.1f, 10.0f, "cl_crosshairthickness" );
+	m_pCrosshairDot = new CCvarToggleCheckButton( m_pOptionsPanel, "CrosshairDot", "#GameUI_CrosshairDot", "cl_crosshairdot" );
+	m_pCrosshairColorR = new CCvarSlider( m_pOptionsPanel, "CrosshairColorR", "#GameUI_Crosshair_Color_R", 0.0f, 255.0f, "cl_crosshaircolor_r" );
+	m_pCrosshairColorG = new CCvarSlider( m_pOptionsPanel, "CrosshairColorG", "#GameUI_Crosshair_Color_G", 0.0f, 255.0f, "cl_crosshaircolor_g" );
+	m_pCrosshairColorB = new CCvarSlider( m_pOptionsPanel, "CrosshairColorB", "#GameUI_Crosshair_Color_B", 0.0f, 255.0f, "cl_crosshaircolor_b" );
+	m_pCrosshairDrawOutline = new CCvarToggleCheckButton( m_pOptionsPanel, "CrosshairDrawOutline", "#GameUI_Crosshair_DrawOutline", "cl_crosshair_drawoutline" );
+	m_pCrosshairOutlineThickness = new CCvarSlider( m_pOptionsPanel, "CrosshairOutlineThickness", "#GameUI_Crosshair_OutlineThickness", 0.1f, 3.0f, "cl_crosshair_outlinethickness" );
+	m_pCrosshairT = new CCvarToggleCheckButton( m_pOptionsPanel, "CrosshairT", "#GameUI_Crosshair_T", "cl_crosshair_t" );
 
-	InitCrosshairColorEntries();
+	//m_pCrosshairStyle->AddItem( "#GameUI_Crosshair_Style_0", "cl_crosshairstyle 0" );
+	//m_pCrosshairStyle->AddItem( "#GameUI_Crosshair_Style_1", "cl_crosshairstyle 1" );
+	m_pCrosshairStyle->AddItem( "#GameUI_Crosshair_Style_2", "cl_crosshairstyle 2" );
+	m_pCrosshairStyle->AddItem( "#GameUI_Crosshair_Style_3", "cl_crosshairstyle 3" );
+	m_pCrosshairStyle->AddItem( "#GameUI_Crosshair_Style_4", "cl_crosshairstyle 4" );
 
-	// TODO: rewrite this shit
-	m_pStyleComboBox->AddItem( "#GameUI_Crosshair_Style_0", "cl_crosshairstyle 0" );
-	m_pStyleComboBox->AddItem( "#GameUI_Crosshair_Style_1", "cl_crosshairstyle 1" );
-	m_pStyleComboBox->AddItem( "#GameUI_Crosshair_Style_2", "cl_crosshairstyle 2" );
-	m_pStyleComboBox->AddItem( "#GameUI_Crosshair_Style_3", "cl_crosshairstyle 3" );
-	m_pStyleComboBox->AddItem( "#GameUI_Crosshair_Style_4", "cl_crosshairstyle 4" );
-	m_pStyleComboBox->AddItem( "#GameUI_Crosshair_Style_5", "cl_crosshairstyle 5" );
-
-	m_pColorComboBox->AddActionSignalTarget( this );
-	m_pStyleComboBox->AddActionSignalTarget( this );
-	m_pOutlineCheckbox->AddActionSignalTarget( this );
-	m_pOutlineSlider->AddActionSignalTarget( this );
+	m_pCrosshairStyle->AddActionSignalTarget( this );
+	m_pCrosshairAlpha->AddActionSignalTarget( this );
+	m_pCrosshairUseAlpha->AddActionSignalTarget( this );
+	m_pCrosshairGap->AddActionSignalTarget( this );
+	m_pCrosshairGapUseWeaponValue->AddActionSignalTarget( this );
+	m_pCrosshairSize->AddActionSignalTarget( this );
+	m_pCrosshairThickness->AddActionSignalTarget( this );
+	m_pCrosshairDot->AddActionSignalTarget( this );
+	m_pCrosshairColorR->AddActionSignalTarget( this );
+	m_pCrosshairColorG->AddActionSignalTarget( this );
+	m_pCrosshairColorB->AddActionSignalTarget( this );
+	m_pCrosshairDrawOutline->AddActionSignalTarget( this );
+	m_pCrosshairOutlineThickness->AddActionSignalTarget( this );
+	m_pCrosshairT->AddActionSignalTarget( this );
 
 	m_iCrosshairTextureID = vgui::surface()->CreateNewTextureID();
 	vgui::surface()->DrawSetTextureFile( m_iCrosshairTextureID, "vgui/white_additive" , true, false);
@@ -144,35 +164,12 @@ CrosshairImagePanelCS::CrosshairImagePanelCS( Panel *parent, const char *name, C
 	ResetData();
 }
 
-void CrosshairImagePanelCS::InitCrosshairColorEntries()
-{
-	if (m_pColorComboBox != NULL)
-	{
-		KeyValues *data = new KeyValues("data");
-
-		// add in the "Default" selection
-		data->Clear();
-
-		// add in the colors for the color list
-		for ( int i = 0; i < NumCrosshairColors; i++ )
-		{
-			data->SetInt("color", i);
-			m_pColorComboBox->AddItem( s_crosshairColors[ i ].name, data);
-		}
-
-		data->SetInt("color", NumCrosshairColors);
-		m_pColorComboBox->AddItem( "Custom", data);
-
-		data->deleteThis();
-	}
-}
-
 //-----------------------------------------------------------------------------
 void CrosshairImagePanelCS::DrawCrosshairRect( int r, int g, int b, int a, int x0, int y0, int x1, int y1, bool bAdditive )
 {
-	if ( m_pOutlineCheckbox->IsSelected() )
+	if ( m_pCrosshairDrawOutline->IsSelected() )
 	{
-		float flThick = m_pOutlineSlider->GetSliderValue();
+		float flThick = m_pCrosshairOutlineThickness->GetSliderValue();
 		vgui::surface()->DrawSetColor( 0, 0, 0, a );
 		vgui::surface()->DrawFilledRect( x0-flThick, y0-flThick, x1+flThick, y1+flThick );
 	}
@@ -201,14 +198,21 @@ void CrosshairImagePanelCS::Paint()
 	int wide, tall;
 	GetSize( wide, tall );
 
-	bool bAdditive = !m_pAlphaCheckbox->IsSelected();
-	bool bDynamic = m_pDynamicCheckbox->IsSelected();
+	bool bAdditive = !m_pCrosshairUseAlpha->IsSelected();
+	bool bDynamic = /*(m_pCrosshairStyle->GetActiveItem() != 1) &&
+					(m_pCrosshairStyle->GetActiveItem() != 4);*/
+					m_pCrosshairStyle->GetActiveItem() != 2;
 
 	int a = 255;
 	if ( !bAdditive )
-		a = m_pColorAlphaSlider->GetSliderValue();
+		a = m_pCrosshairAlpha->GetSliderValue();
 
-	vgui::surface()->DrawSetColor( m_R, m_G, m_B, a );
+	int r, g, b;
+	r = m_pCrosshairColorR->GetSliderValue();
+	g = m_pCrosshairColorG->GetSliderValue();
+	b = m_pCrosshairColorB->GetSliderValue();
+
+	vgui::surface()->DrawSetColor( m_pCrosshairColorR->GetSliderValue(), m_pCrosshairColorG->GetSliderValue(), m_pCrosshairColorB->GetSliderValue(), a );
 
 	if ( bAdditive )
 	{
@@ -218,8 +222,8 @@ void CrosshairImagePanelCS::Paint()
 	int centerX = wide / 2;
 	int centerY = tall / 2;
 
-	int iBarSize = RoundFloatToInt(m_barSize * screenTall / 480.0f);
-	int iBarThickness = max(1, RoundFloatToInt(m_barThickness * (float)screenTall / 480.0f));
+	int iBarSize = RoundFloatToInt(m_pCrosshairSize->GetSliderValue() * screenTall / 480.0f);
+	int iBarThickness = max(1, RoundFloatToInt(m_pCrosshairThickness->GetSliderValue() * (float)screenTall / 480.0f));
 
 	float fBarGap = 4.0f;
 	if ( bDynamic )
@@ -228,36 +232,65 @@ void CrosshairImagePanelCS::Paint()
 		fBarGap *= (1.0f + cosf(curtime * 1.5f) * 0.5f);
 	}
 
-	int iBarGap = RoundFloatToInt(fBarGap * screenTall / 480.0f);
+	int iBarSizeInner = iBarSize;
+
+	// draw the crosshair that splits off from the main xhair
+	if ( m_pCrosshairStyle->GetActiveItem() == 2 )
+	{
+		iBarSizeInner = ceil( (float) iBarSize * (1.0f - 0.35f) ); // 0.35f = cl_crosshair_dynamic_maxdist_splitratio.GetFloat()
+		int iBarSizeOuter = floor( (float) iBarSize * 0.35f ); // 0.35f = cl_crosshair_dynamic_maxdist_splitratio.GetFloat()
+
+		// draw horizontal crosshair lines
+		int iInnerLeft = (centerX - m_pCrosshairGap->GetSliderValue() - iBarThickness / 2) - iBarSizeInner;
+		int iInnerRight = iInnerLeft + 2 * (m_pCrosshairGap->GetSliderValue() + iBarSizeInner) + iBarThickness;
+		int iOuterLeft = iInnerLeft - iBarSizeOuter;
+		int iOuterRight = iInnerRight + iBarSizeOuter;
+		int y0 = centerY - iBarThickness / 2;
+		int y1 = y0 + iBarThickness;
+		DrawCrosshairRect( r, g, b, a, iOuterLeft, y0, iInnerLeft, y1, bAdditive );
+		DrawCrosshairRect( r, g, b, a, iInnerRight, y0, iOuterRight, y1, bAdditive );
+
+		// draw vertical crosshair lines
+		int iInnerTop = (centerY - m_pCrosshairGap->GetSliderValue() - iBarThickness / 2) - iBarSizeInner;
+		int iInnerBottom = iInnerTop + 2 * (m_pCrosshairGap->GetSliderValue() + iBarSizeInner) + iBarThickness;
+		int iOuterTop = iInnerTop - iBarSizeOuter;
+		int iOuterBottom = iInnerBottom + iBarSizeOuter;
+		int x0 = centerX - iBarThickness / 2;
+		int x1 = x0 + iBarThickness;
+		if ( !m_pCrosshairT->IsSelected() )
+			DrawCrosshairRect( r, g, b, a, x0, iOuterTop, x1, iInnerTop, bAdditive );
+		DrawCrosshairRect( r, g, b, a, x0, iInnerBottom, x1, iOuterBottom, bAdditive );
+	}
 
 	// draw horizontal crosshair lines
-	int iInnerLeft	= centerX - iBarGap - iBarThickness / 2;
-	int iInnerRight	= iInnerLeft + 2 * iBarGap + iBarThickness;
-	int iOuterLeft	= iInnerLeft - iBarSize;
-	int iOuterRight	= iInnerRight + iBarSize;
+	int iInnerLeft = centerX - m_pCrosshairGap->GetSliderValue() - iBarThickness / 2;
+	int iInnerRight = iInnerLeft + 2 * m_pCrosshairGap->GetSliderValue() + iBarThickness;
+	int iOuterLeft = iInnerLeft - iBarSizeInner;
+	int iOuterRight = iInnerRight + iBarSizeInner;
 	int y0 = centerY - iBarThickness / 2;
 	int y1 = y0 + iBarThickness;
-	DrawCrosshairRect( iOuterLeft, y0, iInnerLeft, y1, bAdditive );
-	DrawCrosshairRect( iInnerRight, y0, iOuterRight, y1, bAdditive );
+	DrawCrosshairRect( r, g, b, a, iOuterLeft, y0, iInnerLeft, y1, bAdditive );
+	DrawCrosshairRect( r, g, b, a, iInnerRight, y0, iOuterRight, y1, bAdditive );
 
 	// draw vertical crosshair lines
-	int iInnerTop		= centerY - iBarGap - iBarThickness / 2;
-	int iInnerBottom	= iInnerTop + 2 * iBarGap + iBarThickness;
-	int iOuterTop		= iInnerTop - iBarSize;
-	int iOuterBottom	= iInnerBottom + iBarSize;
+	int iInnerTop = centerY - m_pCrosshairGap->GetSliderValue() - iBarThickness / 2;
+	int iInnerBottom = iInnerTop + 2 * m_pCrosshairGap->GetSliderValue() + iBarThickness;
+	int iOuterTop = iInnerTop - iBarSizeInner;
+	int iOuterBottom = iInnerBottom + iBarSizeInner;
 	int x0 = centerX - iBarThickness / 2;
 	int x1 = x0 + iBarThickness;
-	DrawCrosshairRect( x0, iOuterTop, x1, iInnerTop, bAdditive );
-	DrawCrosshairRect( x0, iInnerBottom, x1, iOuterBottom, bAdditive );
+	if ( !m_pCrosshairT->IsSelected() )
+		DrawCrosshairRect( r, g, b, a, x0, iOuterTop, x1, iInnerTop, bAdditive );
+	DrawCrosshairRect( r, g, b, a, x0, iInnerBottom, x1, iOuterBottom, bAdditive );
 
 	// draw dot
-	if ( m_pDotCheckbox->IsSelected() )
+	if ( m_pCrosshairDot->IsSelected() )
 	{
 		x0 = centerX - iBarThickness / 2;
 		x1 = x0 + iBarThickness;
 		y0 = centerY - iBarThickness / 2;
 		y1 = y0 + iBarThickness;
-		DrawCrosshairRect( x0, y0, x1, y1, bAdditive );
+		DrawCrosshairRect( r, g, b, a, x0, y0, x1, y1, bAdditive );
 	}
 }
 
@@ -268,49 +301,11 @@ void CrosshairImagePanelCS::Paint()
 //-----------------------------------------------------------------------------
 void CrosshairImagePanelCS::UpdateCrosshair()
 {
-	// get the color selected in the combo box.
-	KeyValues *data = m_pColorComboBox->GetActiveItemUserData();
-	int colorIndex = data->GetInt("color");
-	colorIndex = clamp( colorIndex, 0, NumCrosshairColors + 1 );
-
-	int actualVal = 0;
-	int selectedColor = m_pColorComboBox->GetActiveItem();
-
-	ConVarRef cl_crosshaircolor( "cl_crosshaircolor", true );
-	if ( cl_crosshaircolor.IsValid() )
-	{
-		actualVal = clamp( cl_crosshaircolor.GetInt(), 0, NumCrosshairColors + 1 );
-	}
-
-	if ( selectedColor != NumCrosshairColors )	// not custom
-	{
-		m_R = s_crosshairColors[selectedColor].r;
-		m_G = s_crosshairColors[selectedColor].g;
-		m_B = s_crosshairColors[selectedColor].b;
-		m_pColorRSlider->SetSliderValue(m_R);
-		m_pColorGSlider->SetSliderValue(m_G);
-		m_pColorBSlider->SetSliderValue(m_B);
-	}
-	else
-	{
-		m_R = clamp( m_pColorRSlider->GetSliderValue(), 0, 255 );
-		m_G = clamp( m_pColorGSlider->GetSliderValue(), 0, 255 );
-		m_B = clamp( m_pColorBSlider->GetSliderValue(), 0, 255 );
-	}
-
-	m_barSize = m_pSizeSlider->GetSliderValue();
-	m_barThickness = m_pThicknessSlider->GetSliderValue();
 }
 
 
 void CrosshairImagePanelCS::OnSliderMoved(KeyValues *data)
 {
-	vgui::Panel* pPanel = static_cast<vgui::Panel*>(data->GetPtr("panel"));
-
-	if ( pPanel == m_pColorRSlider || pPanel == m_pColorGSlider || pPanel == m_pColorBSlider )
-	{
-		m_pColorComboBox->ActivateItem(NumCrosshairColors);
-	}
 	m_pOptionsPanel->OnControlModified();
 
 	UpdateCrosshair();
@@ -328,56 +323,52 @@ void CrosshairImagePanelCS::OnTextChanged(vgui::Panel *panel)
 
 void CrosshairImagePanelCS::OnCheckButtonChecked()
 {
-	m_pColorAlphaSlider->SetEnabled(m_pAlphaCheckbox->IsSelected());
+	m_pCrosshairAlpha->SetEnabled(m_pCrosshairUseAlpha->IsSelected());
 	m_pOptionsPanel->OnControlModified();
 	UpdateCrosshair();
 }
 
 void CrosshairImagePanelCS::ResetData()
 {
-	// parse the string for the custom color settings and get the initial settings.
-	ConVarRef cl_crosshaircolor( "cl_crosshaircolor", true );
-	int index = 0;
-	if ( cl_crosshaircolor.IsValid() )
-	{
-		index = clamp( cl_crosshaircolor.GetInt(), 0, NumCrosshairColors + 1);
-	}
-	m_pColorComboBox->ActivateItemByRow(index);
+	ConVarRef cl_crosshairstyle( "cl_crosshairstyle", true );
+	m_pCrosshairStyle->SetInitialItem( cl_crosshairstyle.GetInt() );
 
-	m_pAlphaCheckbox->Reset();
-	m_pDynamicCheckbox->Reset();
-	m_pDotCheckbox->Reset();
-	m_pColorRSlider->Reset();
-	m_pColorGSlider->Reset();
-	m_pColorBSlider->Reset();
-	m_pColorAlphaSlider->Reset();
-	m_pSizeSlider->Reset();
-	m_pThicknessSlider->Reset();
+	m_pCrosshairStyle->Reset();
+	m_pCrosshairAlpha->Reset();
+	m_pCrosshairUseAlpha->Reset();
+	m_pCrosshairGap->Reset();
+	m_pCrosshairGapUseWeaponValue->Reset();
+	m_pCrosshairSize->Reset();
+	m_pCrosshairThickness->Reset();
+	m_pCrosshairDot->Reset();
+	m_pCrosshairColorR->Reset();
+	m_pCrosshairColorG->Reset();
+	m_pCrosshairColorB->Reset();
+	m_pCrosshairDrawOutline->Reset();
+	m_pCrosshairOutlineThickness->Reset();
+	m_pCrosshairT->Reset();
+
+	SetImage( crosshairBackgroundImages[RandomInt(0, ARRAYSIZE(crosshairBackgroundImages) - 1)] ); // bruh
 
 	UpdateCrosshair();
 }
 
 void CrosshairImagePanelCS::ApplyChanges()
 {
-	m_pAlphaCheckbox->ApplyChanges();
-	m_pDynamicCheckbox->ApplyChanges();
-	m_pDotCheckbox->ApplyChanges();
-	m_pColorRSlider->ApplyChanges();
-	m_pColorGSlider->ApplyChanges();
- 	m_pColorBSlider->ApplyChanges();
-	m_pColorAlphaSlider->ApplyChanges();
-	m_pSizeSlider->ApplyChanges();
-	m_pThicknessSlider->ApplyChanges();
-
-	char cmd[256];
-	cmd[0] = 0;
-
-	if (m_pColorComboBox != NULL)
-	{
-		int val = m_pColorComboBox->GetActiveItem();
-		Q_snprintf( cmd, sizeof(cmd), "cl_crosshaircolor %d\n", val );
-		engine->ClientCmd_Unrestricted( cmd );
-	}
+	m_pCrosshairStyle->ApplyChanges();
+	m_pCrosshairAlpha->ApplyChanges();
+	m_pCrosshairUseAlpha->ApplyChanges();
+	m_pCrosshairGap->ApplyChanges();
+	m_pCrosshairGapUseWeaponValue->ApplyChanges();
+	m_pCrosshairSize->ApplyChanges();
+	m_pCrosshairThickness->ApplyChanges();
+	m_pCrosshairDot->ApplyChanges();
+	m_pCrosshairColorR->ApplyChanges();
+	m_pCrosshairColorG->ApplyChanges();
+	m_pCrosshairColorB->ApplyChanges();
+	m_pCrosshairDrawOutline->ApplyChanges();
+	m_pCrosshairOutlineThickness->ApplyChanges();
+	m_pCrosshairT->ApplyChanges();
 }
 
 //-----------------------------------------------------------------------------
