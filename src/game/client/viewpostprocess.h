0e81bb2899fadbc5a8530f652f674099dbe2af4e
PiMoNFeeD <36343425+PiMoNFeeD@users.noreply.github.com>
2021-02-23 17:59:49 +0300

    restoring ironsight dot which somehow broke

--- src/game/client/viewpostprocess.h
+++ src/game/client/viewpostprocess.h
@@ -11,6 +11,10 @@
 #pragma once
 #endif
 
+#ifdef CSTRIKE_DLL
+#include "cs_shareddefs.h"
+#endif
+
 void DoEnginePostProcessing( int x, int y, int w, int h, bool bFlashlightIsOn, bool bPostVGui = false );
 void DoImageSpaceMotionBlur( const CViewSetup &view, int x, int y, int w, int h );
 void DumpTGAofRenderTarget( const int width, const int height, const char *pFilename );
