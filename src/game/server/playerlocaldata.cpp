42825899f964d465d94b0fa8463e45c0d008ec70
PiMoNFeeD <zarod2005@yandex.ru>
2021-08-19 05:33:55 +0300

    fixed free look spectator mode
    WHY DID I REMOVE THE FUCKIN VAR HALF A YEAR AGO
    tldr: it was causing prediction go brrr and all interpolated vars were reset cuz network value and predicted value wasn't matching

--- src/game/server/playerlocaldata.cpp
+++ src/game/server/playerlocaldata.cpp
@@ -50,6 +50,7 @@ BEGIN_SEND_TABLE_NOBASE( CPlayerLocalData, DT_Local )
 	SendPropFloat		( SENDINFO_VECTORELEM(m_aimPunchAngleVel, 2), 32, SPROP_NOSCALE|SPROP_CHANGES_OFTEN ),
 
 #else
+	SendPropFloat	(SENDINFO(m_flFallVelocity), 17, SPROP_CHANGES_OFTEN, -4096.0f, 4096.0f ),
 	SendPropVector	(SENDINFO(m_viewPunchAngle),		-1,  SPROP_COORD|SPROP_CHANGES_OFTEN),
 	SendPropVector	(SENDINFO(m_aimPunchAngle),			-1,  SPROP_COORD|SPROP_CHANGES_OFTEN),
 	SendPropVector	(SENDINFO(m_aimPunchAngleVel),      -1,  SPROP_COORD|SPROP_CHANGES_OFTEN),
